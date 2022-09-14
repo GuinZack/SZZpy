3aa76fa51faaa37d42459d06ac5663209bfdd625
nayeawon <yea1189@gmail.com>
2022-09-15 03:09:58 +0900

    [REFACTOR] csv format

--- app/src/main/java/hotdog/utils/JsonConverter.java
+++ app/src/main/java/hotdog/utils/JsonConverter.java
@@ -64,7 +64,8 @@ public class JsonConverter {
                     //                              ["return np.linalg.norm(faces - face_to_compare, axis=1)"]]
                     String [] path_hash = tuple.toString().split(", ");
                     path_hash[0] = path_hash[0].replace("[",""); // FilePath
-                    path_hash[2] = path_hash[2].replace("]",""); // Line
+                    path_hash[2] = path_hash[2].replace("]","")
+                                                .replace("[", ""); // Line
                     out.println(j.repo_name + "," + path_hash[1] + "," + j.fix_commit_hash + ","
                                     + path_hash[0] + "," + path_hash[2]);
                 }
